8fc2a8a5b067be419cdabea2cf9df7376aa636ae
Philip <philiposun@gmail.com>
2023-10-10 02:04:44 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// javascript class
+
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+}
+
+<p>The example above creates a class named "Car".</p>
+<p>The class has two initial properties: "name" and "year".</p>
