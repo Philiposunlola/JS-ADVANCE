d7bb687aac37e825f36266d9a664d2d9ee27b9fe
Philip <philiposun@gmail.com>
2023-10-10 02:10:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// javascript class
+// Javascript class
 
 class Car {
     constructor(name, year) {
@@ -7,5 +7,19 @@ class Car {
     }
 }
 
-<p>The example above creates a class named "Car".</p>
-<p>The class has two initial properties: "name" and "year".</p>
+//The example above creates a class named "Car".</p>
+//The class has two initial properties: "name" and "year".</p>
+
+<p>Creating two car objects from a car class:</p>
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+  }
+  
+  const myCar1 = new Car("Ford", 2014);
+  const myCar2 = new Car("Audi", 2019);
+  
+  document.getElementById("demo").innerHTML =
+  myCar1.name + " " + myCar2.name;
