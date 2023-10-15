2bb9f9f2d15556df29da37efcef9ef292338590c
Philip <philiposun@gmail.com>
2023-10-15 01:12:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,4 @@ class Car {
   const myCar1 = new Car("Ford", 2014);
   const myCar2 = new Car("Audi", 2019);
   
-  document.getElementById("demo").innerHTML =
-  myCar1.name + " " + myCar2.name;
+  document.getElementById("demo").innerHTML = myCar1.name + " " + myCar2.name;
