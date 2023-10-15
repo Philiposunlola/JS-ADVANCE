b711e50574451922c243471705d4c28b5455acd8
Philip <philiposun@gmail.com>
2023-10-15 01:16:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ class Car {
   const myCar2 = new Car("Audi", 2019);
   
   document.getElementById("demo").innerHTML = myCar1.name + " " + myCar2.name;
+
+  class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+  
+    }
+    age() {
+      const date = new Date();
+      return date.getFullYear() - this.year;
+    }
+  }
+  
+  const myCar = new Car("Ford", 2014);
+  document.getElementById("demo1").innerHTML =
+  "My car is " + myCar.age() + " years old.";
